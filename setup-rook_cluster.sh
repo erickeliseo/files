fe73410abd8a475fe78fcd2ed061cf75194c8c3d
Erick <erick.eliseo@gmail.com>
2018-10-03 11:57:23 -0600

    Changes

--- setup-rook_cluster.sh
+++ setup-rook_cluster.sh
@@ -8,17 +8,17 @@
 
 # Instalando Rook y Habilitando el Monitoreo
 echo -e "\u001b[32mWait for Kubernetes and Rook to be ready\u001b[m\r\n"
-kubectl create -f rook/cluster/examples/kubernetes/ceph/operator.yaml
-kubectl create -f rook/cluster/examples/kubernetes/ceph/cluster.yaml
+kubectl create -f ~/rook/cluster/examples/kubernetes/ceph/operator.yaml
+kubectl create -f ~/rook/cluster/examples/kubernetes/ceph/cluster.yaml
 
 # Instalando Prometheus
-kubectl create -f files/dashboard-external-NodePort.yaml
+kubectl create -f ~/files/dashboard-external-NodePort.yaml
 kubectl create -f https://raw.githubusercontent.com/coreos/prometheus-operator/release-0.15/bundle.yaml
-kubectl create -f rook/cluster/examples/kubernetes/monitoring/service-monitor.yaml
-kubectl create -f rook/cluster/examples/kubernetes/monitoring/prometheus.yaml
-kubectl create -f rook/cluster/examples/kubernetes/monitoring/prometheus-service.yaml
+kubectl create -f ~/rook/cluster/examples/kubernetes/monitoring/service-monitor.yaml
+kubectl create -f ~/rook/cluster/examples/kubernetes/monitoring/prometheus.yaml
+kubectl create -f ~/rook/cluster/examples/kubernetes/monitoring/prometheus-service.yaml
 
 ## Instalando Grafana
-kubectl create -f files/grafana-external-NodePort.yaml
+kubectl create -f ~/files/grafana-external-NodePort.yaml
 helm init
-helm install --name grafana-rook-cluster stable/grafana -f grafana-helm-values.yaml
+helm install --name grafana-rook-cluster stable/grafana -f ~/files/grafana-helm-values.yaml
