362f34bfce1bbc1e153a50b4cf6bdac04c216e93
Erick <erick.eliseo@gmail.com>
2018-10-05 15:09:00 -0600

    Changes

--- setup-rook_cluster.sh
+++ setup-rook_cluster.sh
@@ -27,7 +27,7 @@ sleep 15
 kubectl create -f ~/rook/cluster/examples/kubernetes/monitoring/prometheus.yaml
 sleep 15
 kubectl create -f ~/rook/cluster/examples/kubernetes/monitoring/prometheus-service.yaml
-sleep 60
+sleep 150
 # Extraer configuracion de Prometheus e insertar en ~/files/grafana-helm-values.yaml
 export URL=http://"$(kubectl -n rook-ceph -o jsonpath={.status.hostIP} get pod prometheus-rook-prometheus-0):9090"
 sed -i "s|url:|url: $URL|g" ~/files/grafana-helm-values.yaml
@@ -36,23 +36,28 @@ sleep 5
 echo -e "\u001b[32mGrafana\u001b[m\r\n"
 kubectl create -f ~/files/grafana-external-NodePort.yaml
 helm install --name grafana-rook-cluster stable/grafana -f ~/files/grafana-helm-values.yaml
-sleep 150
+sleep 300
 # Extraer POD y URL de Grafana
+# Otra forma de Exportar el nombre del POD: export POD_NAME=$(kubectl get pods --namespace default -l "app=grafana-rook-cluster,component=" -o jsonpath="{.items[0].metadata.name}")
 export PODGRAFANA=$(kubectl get pods | grep grafana-rook-cluster | awk '{print $1}')
 export URLGRAFANA=http://"$(kubectl  -o jsonpath={.status.hostIP} get pod "$(kubectl get pods | grep grafana-rook-cluster | awk '{print $1}')")":3000
 
 # Instalando Dashboard de Rook en Grafana
 # Descarga el JSON del Dashboard 2842
-kubectl exec -it $PODGRAFANA  curl https://grafana.com/api/dashboards/2842/revisions/7/download > /tmp/grafana-dashboard-Ceph-Cluster-2842.json
+# De esta forma no funciono el Katacoda: kubectl exec -it $PODGRAFANA  curl https://grafana.com/api/dashboards/2842/revisions/7/download > /tmp/grafana-dashboard-Ceph-Cluster-2842.json
+kubectl exec -it $PODGRAFANA  -- bash -c "curl https://grafana.com/api/dashboards/2842/revisions/7/download > /tmp/grafana-dashboard-Ceph-Cluster-2842.json"
+sleep 100
 
 # Elimina la primera linea /tmp/grafana-dashboard-Ceph-Cluster-2842.json
-kubectl exec -it $PODGRAFANA sed -i '1d' /tmp/grafana-dashboard-Ceph-Cluster-2842.json
+kubectl exec -it $PODGRAFANA -- bash -c "sed -i '1d' /tmp/grafana-dashboard-Ceph-Cluster-2842.json"
 
 # Inserta linea /tmp/grafana-dashboard-Ceph-Cluster-2842.json
-kubectl exec -it $PODGRAFANA sed -i '1i "dashboard": {' /tmp/grafana-dashboard-Ceph-Cluster-2842.json
+kubectl exec -it $PODGRAFANA -- bash -c "sed -i '1i \"\dashboard\"\: {' /tmp/grafana-dashboard-Ceph-Cluster-2842.json"
 
 # Inserta linea /tmp/grafana-dashboard-Ceph-Cluster-2842.json
-kubectl exec -it $PODGRAFANA sed -i '1i {' /tmp/grafana-dashboard-Ceph-Cluster-2842.json
+kubectl exec -it $PODGRAFANA -- bash -c "sed -i '1i {' /tmp/grafana-dashboard-Ceph-Cluster-2842.json"
 
 # Inserta linea /tmp/grafana-dashboard-Ceph-Cluster-2842.json
-kubectl exec -it $PODGRAFANA echo "}" >> /tmp/grafana-dashboard-Ceph-Cluster-2842.json
+kubectl exec -it $PODGRAFANA -- bash -c "echo "}" >> /tmp/grafana-dashboard-Ceph-Cluster-2842.json"
+
+kubectl exec -it $PODGRAFANA -- bash -c "cd /tmp ; curl --user admin:strongpassword 'http://localhost:3000/api/dashboards/db' -X POST -H 'Content-Type:application/json;charset=UTF-8' --data-binary @./grafana-dashboard-Ceph-Cluster-2842.json"
